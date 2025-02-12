fb2160945c9e792954e7df1b28ed396eb37b0ac8
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-02-13 00:46:40 +0700

    feat: centrallized api calling throught api_service with exception handing

---
+++ src/service/api_service.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const apiService = axios.create({
+  baseURL: String(import.meta.env.VITE_BACKEND_URL) || "http://localhost:8080",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+//Request interceptor
+apiService.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//Response interceptor
+apiService.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response) {
+      const { status } = error.response;
+      if (status === 401) {
+        //toast.error("You are not authorized to access this resource.");
+      } else if (status === 500) {
+        //toast.error("Internal server error.");
+      }
+    } else if (error.request) {
+      //toast.error("Network error.");
+    } else {
+      //toast.error("Something went wrong.");
+    }
+  }
+);
+
+export default apiService;

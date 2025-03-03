2320f7f033c058a87afe283becf6d45b388609d2
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-03-04 01:16:35 +0700

    feat: add core page template

---
+++ src/router/Router.tsx
@@ -0,0 +1,40 @@
+import LoadingScreen from "@/custom_components/side/LoadingScreen";
+import { lazy, LazyExoticComponent, Suspense } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { JSX } from "react/jsx-runtime";
+
+const Loadable = (Component: LazyExoticComponent<() => JSX.Element>) => {
+  return (
+    <Suspense fallback={<LoadingScreen />}>
+      <Component />
+    </Suspense>
+  );
+};
+
+const LoginPage = Loadable(
+  lazy(() => import("@/custom_components/page/LoginPage"))
+);
+const RegisterPage = Loadable(
+  lazy(() => import("@/custom_components/page/RegisterPage"))
+);
+const HomePage = Loadable(
+  lazy(() => import("@/custom_components/page/HomePage"))
+);
+const NotFoundPage = Loadable(
+  lazy(() => import("@/custom_components/page/NotFoundPage"))
+);
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={"/homepage"} replace />} />
+      <Route path="/homepage" index element={HomePage} />
+      <Route path="/login" element={LoginPage} />
+      <Route path="/register" element={RegisterPage} />
+      <Route path="/not-found" element={NotFoundPage} />
+      <Route path="*" element={<Navigate to={"/not-found"} replace />} />
+    </Routes>
+  );
+};
+
+export default Router;
